2b408feaa5c505c66578573c30f61abc2fa9e86c
Kevin Francis Becker <37177241+KebecStatoil@users.noreply.github.com>
2019-11-04 23:31:41 +0100

    Create GeneratePipelineDeployment.ps1

---
+++ GeneratePipelineDeployment.ps1
@@ -0,0 +1,27 @@
+$SolutionPath = "C:\Users\kebec\Documents\GitHub\DataLakers\projects\SafranProject\5.0"
+
+$LinkedServiceTemplate = "if ((Get-AzureRmDataFactoryV2LinkedService -ResourceGroupName $rgname -DataFactoryName $dfname | Where-Object {{$_.Name -eq '{0}'}}).Count -eq 0){{`r`n    Set-AzureRmDataFactoryV2LinkedService -ResourceGroupName $rgname -DataFactoryName $dfname -Name `"{0}`" -DefinitionFile `"$workDir\LinkedServices\{0}.json`" -Verbose -ErrorAction Stop;`r`n}}"
+$LinkedServices = Get-ChildItem -Path (Join-Path $SolutionPath \DF\linkedServices) | ForEach-Object { $LinkedServiceTemplate -f $_.BaseName }
+
+Write-Output $LinkedServices
+
+Write-Output ""
+
+$DatasetTemplate = "Set-AzureRmDataFactoryV2Dataset -ResourceGroupName $rgname -DataFactoryName $dfname -Name `"{0}`" -DefinitionFile `"$workDir\Datasets\{0}.json`" -Verbose -ErrorAction Stop;"
+$Datasets = Get-ChildItem -Path (Join-Path $SolutionPath \DF\Datasets) | ForEach-Object { $DatasetTemplate -f $_.BaseName }
+
+Write-Output $Datasets
+
+Write-Output ""
+
+$PipelineTemplate = "Set-AzureRmDataFactoryV2Pipeline -ResourceGroupName $rgname -DataFactoryName $dfname -Name `"{0}`" -DefinitionFile `"$workDir\Pipelines\{0}.json`" -Verbose -ErrorAction Stop;"
+$Pipelines = Get-ChildItem -Path (Join-Path $SolutionPath \DF\Pipelines) | ForEach-Object { $PipelineTemplate -f $_.BaseName }
+
+Write-Output $Pipelines
+
+Write-Output ""
+
+$TriggerTemplate = "Set-AzureRmDataFactoryV2Trigger -ResourceGroupName $rgname -DataFactoryName $dfname -Name `"{0}`" -DefinitionFile `"$workDir\Triggers\{0}.json`" -Verbose -ErrorAction Stop;"
+$Triggers = Get-ChildItem -Path (Join-Path $SolutionPath \DF\Triggers) | ForEach-Object { $TriggerTemplate -f $_.BaseName }
+
+Write-Output $Triggers
