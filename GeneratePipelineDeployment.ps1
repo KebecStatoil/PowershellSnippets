36296a6f43ba9bd85872e7bdba2cdcd5a6738faa
Kevin Francis Becker <37177241+KebecStatoil@users.noreply.github.com>
2019-11-06 20:47:32 +0100

    Update GeneratePipelineDeployment.ps1

--- GeneratePipelineDeployment.ps1
+++ GeneratePipelineDeployment.ps1
@@ -1,6 +1,6 @@
 $SolutionPath = "C:\Users\kebec\Documents\GitHub\DataLakers\projects\SafranProject\5.0"
 
-$LinkedServiceTemplate = "if ((Get-AzureRmDataFactoryV2LinkedService -ResourceGroupName `$rgname -DataFactoryName `$dfname | Where-Object {{$_.Name -eq '{0}'}}).Count -eq 0){{`r`n    Set-AzureRmDataFactoryV2LinkedService -ResourceGroupName `$rgname -DataFactoryName `$dfname -Name `"{0}`" -DefinitionFile `"`$workDir\LinkedServices\{0}.json`" -Verbose -ErrorAction Stop;`r`n}}"
+$LinkedServiceTemplate = "if ((Get-AzureRmDataFactoryV2LinkedService -ResourceGroupName `$rgname -DataFactoryName `$dfname | Where-Object {{`$_.Name -eq '{0}'}}).Count -eq 0){{`r`n    Set-AzureRmDataFactoryV2LinkedService -ResourceGroupName `$rgname -DataFactoryName `$dfname -Name `"{0}`" -DefinitionFile `"`$workDir\LinkedServices\{0}.json`" -Verbose -ErrorAction Stop;`r`n}}"
 $LinkedServices = Get-ChildItem -Path (Join-Path $SolutionPath \DF\linkedServices) | ForEach-Object { $LinkedServiceTemplate -f $_.BaseName }
 
 Write-Output $LinkedServices
